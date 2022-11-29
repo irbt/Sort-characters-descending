8e89452951017005b3b27791efd4cf796ef25a0c
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-29 14:01:26 +0100

    works

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿//Given a string, write a method that returns array of chars (ASCII characters) sorted in descending order.
+namespace Program
+{
+    class sortedCharTask
+    {
+        public static void Main(string[] args)
+        {
+
+            static void SortCharactersDescending(string s)
+            {
+                int len = s.Length;
+                char [] tab = new char [len];
+                tab= s.ToCharArray();
+                Array.Sort(tab);
+                Array.Reverse(tab);
+                for (int i = 0; i < len; i++)
+                {                    
+                Console.Write(tab[i]);
+                }
+                Console.WriteLine();
+            }
+
+
+
+            SortCharactersDescending("onomatopoeia");//→ tpoooonmieaa
+            SortCharactersDescending("fohjwf42os");// → wsoojhff42
+        }
+    }
+}
